8b77b36cd63daa9a45c2de98c092364221405beb
Lucasjrt <lucas.teixeira@neppoadm.com.br>
2020-11-28 23:00:04 -0300

    Adding start of the skeleton

---
+++ automaton.h
@@ -0,0 +1,10 @@
+#ifndef AUTOMATON_H_DEFINED
+#define AUTOMATON_H_DEFINED
+
+struct automaton {
+
+};
+
+typedef struct automaton Automaton;
+
+#endif
--- lexical_analyzer.c
+++ lexical_analyzer.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-    printf("Hello World!\n");
+    char source_code[128];
+    char automaton[128];
+    if (argc != 3) {
+        printf("Usage: <application name> <source code path> <automaton path>\n");
+    }
+    strncpy(source_code, argv[1], 128);
+    strncpy(automaton, argv[2], 128);
+    printf("Parsing %s source code using %s automaton\n", source_code, automaton);
+
     return EXIT_SUCCESS;
 }
